105770af4f5d82e14c9bfb95e843e5158eaccabe
AlMahdi68 <hamedelbadawy204@gmail.com>
2025-06-13 08:12:20 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
 // Supabase credentials for your project
-const SUPABASE_URL = "https://jaqjxmttwwcenndrsizq.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphcWp4bXR0d3djZW5uZHJzaXpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NzU5MjgsImV4cCI6MjA2NTM1MTkyOH0.PjDBZk79fHnmMgKhtSTo_BtkwN9YeGsz9Sd1lZqhTwQ";
+const SUPABASE_URL = "https://fmofmuvhvjqawjcxpofg.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtb2ZtdXZodmpxYXdqY3hwb2ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3ODE5NTEsImV4cCI6MjA2NTM1Nzk1MX0.UL5nlwIUPy8fnYcU8bY32Dd0Zcf7r5QZB5IKuAoggl4";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
